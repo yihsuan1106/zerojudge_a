1c05ebaf0e7ff6f4f45c6e8b56fba3ffd8f94360
yihsuan1106 <97450167+yihsuan1106@users.noreply.github.com>
2022-01-20 12:03:07 +0800

    a001

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  120
+//
+//  Created by 彭薏璇 on 2022/1/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    char str[1000];
+    scanf("%s",str);
+    printf("hello, %s\n",str);
+    return 0;
+}
